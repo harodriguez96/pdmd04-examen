d65d2b3babc53306708414c1354d172439103a42
haizea <96haizea@gmail.com>
2025-05-25 00:06:24 +0200

    Reaplicando .gitignore

--- src/app/interfaces/mis-interfaces.ts
+++
@@ -1,6 +0,0 @@
-// Creamos una interfaz para almacenar la ruta de la imagen en el disco duro (filepath)
-// y la ruta para visualizarla webviewPath)
-export interface IFoto {
-    filepath: string;
-    webviewPath: string;
-  }
